ab2d71b4e6a1468af383b60e830bc206adbd6cf3
Neral <manuel.tertre@epita.fr>
2018-01-29 23:10:37 +0100

    New functions in detect.c

--- src/core/detect.h
+++ src/core/detect.h
@@ -3,6 +3,9 @@
 
 #include "hand.h"
 
+int isclassical(struct hand *hand);
+int ischiitoi(struct hand *hand);
+int iskokushi(struct hand *hand);
 int isvalid(struct hand *hand);
 
 #endif // _DETECT_H_
