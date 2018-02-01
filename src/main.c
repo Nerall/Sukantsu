7e45ada6a7f415001a6b1c20101b663c03982478
Nicolas Mémeint <nicolas.memeint@epita.fr>
2018-02-01 11:58:10 +0100

    Super main

--- src/main.c
+++ src/main.c
@@ -8,11 +8,14 @@
 // DEBUG FUNCTION
 // Will print an histogram to stdout
 static void print_histo(struct histogram *histo) {
-	printf("\t");
-	for (int i = 0; i < HISTO_INDEX_MAX; ++i) {
-		printf("%d ", histo->cells[i]);
+	printf("- 0 1 2 3 4 5 6 7 8\n");
+	for (int i = 0; i < HISTO_INDEX_MAX;) {
+	  printf("  ");
+	  for (int j = 0; j < 9 && i < HISTO_INDEX_MAX; ++i, ++j) {
+	    printf("%d ", histo->cells[i]);
+	  }
+	  printf("\n");
 	}
-	printf("\n");
 }
 
 // DEBUG FUNCTION
@@ -35,6 +38,28 @@ int main() {
 	printf("\tgroup     : %lu\n", sizeof(struct group));
 	printf("\thand      : %lu\n", sizeof(struct hand));
 
-	
-	
+	struct histogram wall;
+	init_histogram(&wall, 4);
+	struct hand hand;
+	init_hand(&hand);
+	for (int i = 0; i < 13; ++i) {
+	  add_tile_hand(&hand, random_pop_histogram(&wall));
+	  random_pop_histogram(&wall);
+	  random_pop_histogram(&wall);
+	  random_pop_histogram(&wall);
+	}
+        while (wall.nb_tiles > 14) {
+	  histo_index_t randi = random_pop_histogram(&wall);
+	  printf("%u\n", randi);
+	  add_tile_hand(&hand, randi);
+	  print_histo(&hand.histo);
+	  makegroup(&hand);
+	  histo_index_t index;
+	  scanf("%hhu", &index);
+	  remove_tile_hand(&hand, index);
+
+	  random_pop_histogram(&wall);
+	  random_pop_histogram(&wall);
+	  random_pop_histogram(&wall);
+	}
 }
