ca6c35b9b9019f6987afceb0310590ccc57f8f2f
Thibault Buatois <thibault.buatois@epita.fr>
2018-03-23 09:40:28 +0100

    D'autres fonctions pour faciliter la suite

--- src/network/net_client.c
+++ src/network/net_client.c
@@ -20,7 +20,45 @@ int connect_to_server(struct net_client *client)
 		wprintf(L"%s\n", "Fail while connecting");
 		return 0;
 	}
-	wprintf(L"%s%s%hu\n", "Connected to", client->host.address, client->port);
+	wprintf(L"%s%s%s%hu\n", "Connected to ", client->host.address,":",client->port);
+	return 1;
+}
+
+int send_to_server(struct net_client *client, const void* data, size_t size)
+{
+	if (sfTcpSocket_getRemoteAddress(client->socket).address == 0) {
+		wprintf(L"%s\n", "Socket not connected");
+		return 0;
+	}
+	if (sfTcpSocket_send(client->socket, data, size) != sfSocketDone) {
+		wprintf(L"%s\n", "Fail while sending");
+		return 0;
+	}
+	wprintf(L"%s\n\n", "Data sent");
+	return 1;
+}
+
+int receive_from_server(struct net_client *client)
+{
+	if (sfTcpSocket_getRemoteAddress(client->socket).address == 0) {
+		wprintf(L"%s\n", "Socket not connected");
+		return 0;
+	}
+	char buf[256];
+	size_t buflen = 256 * sizeof(char);
+	size_t received = 1;
+	while (received != 0) {
+		sfSocketStatus s = sfTcpSocket_receive(client->socket, buf, buflen, &received);
+		if (s == sfSocketError) {
+			wprintf(L"%s\n", "Fail while receiving");
+			return 0;
+		}
+
+		for (size_t i = 0; i < received; ++i) {
+			wprintf(L"%c", buf[i]);
+		}
+	}
+	wprintf(L"\n%s\n", "Data received");
 	return 1;
 }
 
@@ -28,7 +66,7 @@ void disconnect_from_server(struct net_client *client)
 {
 	sfIpAddress cur_address = sfTcpSocket_getRemoteAddress(client->socket);
 	sfTcpSocket_disconnect(client->socket);
-	wprintf(L"%s%s\n", "Disconnected from", cur_address.address);
+	wprintf(L"%s%s\n", "Disconnected from ", cur_address.address);
 }
 
 void client_buster(struct net_client *client)
@@ -38,36 +76,22 @@ void client_buster(struct net_client *client)
 }
 
 void client_test() {
-	sfTcpSocket *socket = sfTcpSocket_create();
-	sfIpAddress host = sfIpAddress_fromString("perdu.com");
-	sfTime timeout = {microseconds: 60000000};
 
-	if (sfTcpSocket_connect(socket, host, 80, timeout) != sfSocketDone) {
-		wprintf(L"%s\n", "Fail while connecting");
+	struct net_client *client = init_client_from_host_and_port("perdu.com", 80);
+	sfTime timeout = {microseconds: 60000000};
+	client->timeout = timeout;
+
+	if (!connect_to_server(client))
 		return;
-	}
 
 	const char *str = "GET http://perdu.com HTTP/1.0\n\r\n\r";
 	size_t n = strlen(str);
-	if (sfTcpSocket_send(socket, str, n) != sfSocketDone) {
-		wprintf(L"%s\n", "Fail while sending");
+	if (!send_to_server(client, str, n))
 		return;
-	}
 
-	char buf[256];
-	size_t buflen = 256 * sizeof(char);
-	size_t received = 1;
-	while (received != 0) {
-		sfSocketStatus s = sfTcpSocket_receive(socket, buf, buflen, &received);
-		if (s == sfSocketError) {
-			wprintf(L"%s\n", "Fail while receiving");
-			return;
-		}
+	if (!receive_from_server(client))
+		return;
 
-		for (size_t i = 0; i < received; ++i) {
-			wprintf(L"%c", buf[i]);
-		}
-	}
-
-	sfTcpSocket_destroy(socket);
+	disconnect_from_server(client);
+	client_buster(client);
 }
